2344274b6bfc1281513edf8aab7b8a7c5096b951
Gleb <mail@g1eb.com>
2017-02-25 18:57:28 +0100

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{ name }}</h1>`,
+})
+
+export class AppComponent  {
+  name = 'App Component';
+}
