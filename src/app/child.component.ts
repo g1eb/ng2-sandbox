56602aac28b18c320e7396393f98ceafa2eeb066
Gleb <mail@g1eb.com>
2017-03-02 17:48:47 +0100

    Added a button to send out events from child to the parent component.

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -11,6 +11,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <h1>Object: {{ someObject | json }}</h1>
     <h1>String: {{ someString }}</h1>
     <h1>Number: {{ someNumber }}</h1>
+    <button (click)="callOnChange()">Click me</button>
   `,
 })
 
@@ -18,4 +19,10 @@ export class ChildComponent  {
   @Input() someObject: any;
   @Input() someString: string;
   @Input() someNumber: number;
+
+  @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
+
+  callOnChange () {
+    this.onChange.emit(this.someObject);
+  }
 }
